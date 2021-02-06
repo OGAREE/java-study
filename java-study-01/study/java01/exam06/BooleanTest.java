c7a04fad59e79de9844b352738090de30c1192d0
OGAREE <ohkangun@naver.com>
2021-02-06 20:26:14 +0900

    Boolean 예시

---
+++ java-study-01/study/java01/exam06/BooleanTest.java
@@ -0,0 +1,14 @@
+package study.java01.exam06;
+
+public class BooleanTest {
+	public static void main(String[] args) {
+		boolean isT = false;
+		
+		if(isT) {
+			System.out.println("ture값입니다.");
+		}
+		else {
+			System.out.println("false값입니다.");
+		}
+	}
+}
