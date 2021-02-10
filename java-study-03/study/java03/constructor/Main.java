12171bf4cf2b3207914f03f0402cdc5f8fdeb846
OGAREE <ohkangun@naver.com>
2021-02-10 14:41:37 +0900

    생성자(constructor)

---
+++ java-study-03/study/java03/constructor/Main.java
@@ -0,0 +1,11 @@
+package study.java03.constructor;
+
+public class Main {
+	public static void main(String[] args) {
+		People p = new People("홍길동", 100);
+		System.out.println(p);
+		//----------
+		People p1 = new People();
+		
+	}
+}
---
+++ java-study-03/study/java03/constructor/People.java
@@ -0,0 +1,61 @@
+package study.java03.constructor;
+
+/*
+ * 생성자(constructor)
+ * 	클래스명과 동일하고 리턴타입이 없다.(메소드는 아니다!!)
+ * 객체를 초기화할 때 사용한다.
+ * 객체 생성시 최초 한번만 호출된다.
+ * public class People{
+ * 	  public People(){
+ * 
+ *    }
+ * }
+ * 
+ * default 생성자가 존재한다.
+ * default 생성자는 아규먼트가 아무것도 없는 것을 말한다.
+ * public class People{
+ * 		public People(){} //default 생성자
+ * }
+ * 
+ * 생성자 오버로딩을 구현하게 되면, default 생성자는 
+ * 자동으로 생성되지 않기 때문에, 구현해 주어야 한다.
+ * 
+ * 생성자 오버로딩(overloading)
+ * 모양은 똑같으나 아규먼트의 갯수와 타입이 다른 것
+ * 
+ * public class People{
+ * 		public People(){}
+ * 		public People(String name,{}
+ * 		public People(String name, int age){}
+ *	} 
+ * */
+
+public class People {
+	private String name;
+	private int age;
+
+	//생성자
+	public People(String name, int age) {
+		this.name=name;
+		this.age=age;
+	}
+
+	//default 생성자
+	public People() {
+
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "People [name = " + name + " , age = "+ age+ "]";
+	}
+
+}
