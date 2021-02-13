3cfab7f653937988f5768bbdb324ac5f28a2d2ba
OGAREE <ohkangun@naver.com>
2021-02-13 15:05:40 +0900

    오버라이딩이란!

---
+++ java-study-03/study/java03/overriding/Child.java
@@ -0,0 +1,7 @@
+package study.java03.overriding;
+
+public class Child extends Parent {
+	public void eat() {
+		System.out.println("과자를 먹는다.");
+	}
+}
---
+++ java-study-03/study/java03/overriding/Parent.java
@@ -0,0 +1,10 @@
+package study.java03.overriding;
+
+public class Parent {
+	public void sleep() {
+		System.out.println("잠을 잔다");
+	}
+	public void eat() {
+		System.out.println("밥을 먹는다.");
+	}
+}
