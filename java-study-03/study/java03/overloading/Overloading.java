9f7640ce71fd3a6520b678b267b8b7ff42bd5289
OGAREE <ohkangun@naver.com>
2021-02-27 22:37:39 +0900

    오버로딩이란!

--- java-study-03/study/java03/overloading/Overloading.java
+++ java-study-03/study/java03/overloading/Overloading.java
@@ -5,11 +5,11 @@ package study.java03.overloading;
  * - 메소드의 이름과 리턴타입은 같고,
  *   아규먼트 갯수와 타입이 다르면 다른 메소드처럼 행동하는 것
  * */
-public class CoffeeMachine {
+public class Overloading {
 	String coffee;
 	int milk;
 	int price;
-	
+
 	public static void eat(String coffee, int price) {
 		System.out.println(coffee+"를 "+ price+ "원에 먹었어요");
 	}
